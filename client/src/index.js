a56386704cf116082627e07d4c398a139d0fab20
yangju <yangju@gmtc.kr>
2021-02-16 18:22:25 +0900

    시계추가(utilbox)
    시계추가함

--- client/src/index.js
+++ client/src/index.js
@@ -2,29 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './main/App';
 import reportWebVitals from './reportWebVitals';
-
+import './index.css'
 import Reducer from './_reducers'
-import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk'
 
 //스토어 생성
-const createStoredWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
+const createStoredWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider
-    store ={createStoredWithMiddleware(
+    store={createStoredWithMiddleware(
       Reducer,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
   >
-      <React.StrictMode>
-        <App />
-    </React.StrictMode>  
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
-,
+  ,
   document.getElementById('root')
 );
 reportWebVitals();
--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import UtilBox from '../component/Map/Section/UtilBox';
 import LeftNav from '../component/Navbar/LeftNav';
 
 function App() {
   return (
     <div>
-      <LeftNav/>
+      <LeftNav />
+      <UtilBox />
     </div>
   );
 }
+
+
 
 export default App;
