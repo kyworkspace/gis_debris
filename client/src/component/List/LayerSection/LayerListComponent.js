24b98983c42fce93b97be5864f999c34e437a454
ParkKiYoung <ky91zzang@naver.com>
2021-03-04 11:04:07 +0900

    infinite scroll made

--- client/src/component/List/LayerSection/LayerListComponent.js
+++ client/src/component/List/LayerSection/LayerListComponent.js
@@ -1,3 +1,4 @@
+import { message } from 'antd';
 import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 
 function LayerListComponent() {
@@ -12,70 +13,50 @@ function LayerListComponent() {
         })
     )
     const [CountPerPage, setCountPerPage] = useState(8)
-    // const [DisPlayList, setDisPlayList] = useState([])
-    const [pageInterval, setpageInterval] = useState(8)
+    const [DisPlayList, setDisPlayList] = useState([])
     const [CurrentPage, setCurrentPage] = useState(1)
     
     const divRef = useRef();
     const currentPageRef = useRef(1);
-    const DisplayListRef = useRef([]);
-
-    useLayoutEffect(() => {
-        console.log()
-        let addList = wholeList.current.filter((item,idx)=>{
-            if(item.seq_no>=0 && item.seq_no<=CountPerPage){
-                return item;
-            }
-        })
-        // setDisPlayList(addList)
-        DisplayListRef.current = addList;
-    }, [])
-
-    
 
     const displayListLoad=(page)=>{
-        console.log("새로고침")
         let addList = wholeList.current.filter((item,idx)=>{
-            if(item.seq_no>=((page-1)*pageInterval)+1 && item.seq_no<=(pageInterval*page)){
+            if(item.seq_no>=((page-1)*CountPerPage)+1 && item.seq_no<=(CountPerPage*page)){
                 return item;
             }
         })
-        console.log(...DisplayListRef.current,...addList)
-        
-        DisplayListRef.current = [...DisplayListRef.current,...addList]
-
-        return DisplayListRef.current;
+        setDisPlayList([...DisPlayList,...addList])
     }
-    const memoList = useMemo(() => displayListLoad(CurrentPage), [CurrentPage])
+
+    useMemo(() => displayListLoad(currentPageRef.current), [currentPageRef.current])
 
     const infiniteScroll = () => {
-        const { documentElement, body } = document;
         if(divRef.current){
             const st = divRef.current.scrollTop;
             const ch = divRef.current.clientHeight;
             const sh = divRef.current.scrollHeight;
-
             if(st+ch >=sh){
-                console.log("페이지 바뀜")
+                if((((currentPageRef.current-1)*CountPerPage)+1) >= wholeList.current.length){
+                    return;
+                }
                 currentPageRef.current = currentPageRef.current+1;
-                console.log(currentPageRef.current);
                 setCurrentPage(currentPageRef.current)
             }
         }
 
       };
+
     useEffect(() => {
         window.addEventListener("scroll", infiniteScroll, true);
         return () => {
             window.removeEventListener("scroll", infiniteScroll);
         }
     }, [])
-    const renderDiv = memoList.map((item,idx)=>{
-        return <div style={{border:'solid', height:'300px'}} key={idx}>{item.layer_name}</div>
+    const renderDiv = DisPlayList.map((item,idx)=>{
+        return <div style={{border:'solid', height:'300px',backgroundColor:'violet'}} key={idx}>{item.layer_name}</div>
     })
-    
     return (
-        <div ref={divRef} style={{height:'800px', backgroundColor:'violet',overflow:'auto'}}>
+        <div ref={divRef} style={{height:'950px', overflowY:'auto'}} className="infiniteScrollDiv">
             {renderDiv}
         </div>
     )
--- client/src/component/utils/InfiniteScrollComponent.jsx
+++ client/src/component/utils/InfiniteScrollComponent.jsx
@@ -1,61 +1,11 @@
-import { Spin } from 'antd';
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react'
-import InfiniteScroll from 'react-infinite-scroller'
-import PropType from 'prop-types';
-import {Queue} from '../../entities/CommonMethods.js'
+import React from 'react'
 
-const pageQueue = new Queue();
-
-function InfiniteScrollComponent(props) {
-
-    const {CountPerPage,ListContent,LoadNextPage} = props;
-    const [DisplayList, setDisplayList] = useState([]);
-    const [HasMoreItems, setHasMoreItems] = useState(true);
-    const [Loading, setLoading] = useState(false);
-
-    const [PageStart, setPageStart] = useState(0)
-
-    const onLoadItems =(page)=>{
-        pageQueue.enqueue(page)
-        if(Loading) return;
-        setLoading(true);
-        let addList = [...DisplayList];
-        while(pageQueue._arr.length>0){
-            let callPage = pageQueue.dequeue()
-            LoadNextPage(callPage)
-            .then(response=>{
-                if(response.success){
-                    addList.push(...response.objList)
-                    setLoading(false)
-                }
-            })
-        }
-        setDisplayList(addList);
-        setPageStart(page)
-        if(addList.length<=CountPerPage*page){
-            setHasMoreItems(false)
-        }
-    }
-    const loader = <div className="demo-loading-container"> <Spin /> </div>;
-    
+function InfiniteScrollComponent() {
     return (
-        <InfiniteScroll
-            pageStart={PageStart}
-            loadMore={onLoadItems}
-            hasMore={HasMoreItems}
-            loader={loader}
-            useWindow={false}
-        >
-            {ListContent(DisplayList)}
-        </InfiniteScroll>
+        <div>
+            
+        </div>
     )
-}
-
-InfiniteScrollComponent.propTypes={
-    ContentList:PropType.array,
-    CountPerPage:PropType.number,
-    IndexColumn:PropType.string,
-    ListContent:PropType.func
 }
 
 export default InfiniteScrollComponent
